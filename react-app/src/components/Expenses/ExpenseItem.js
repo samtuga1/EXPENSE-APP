5a3ae754eb7fe1eac59a2592c32985d5425898e0
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-06-29 11:12:38 +0000

    update

---
+++ react-app/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,18 @@
+import "./ExpenseItem.css";
+import Card from"./UI/Card";
+import ExpenseDate from "./ExpenseDate";
+
+function ExpenseItem(props) {
+  
+  return (
+    <Card className="expense-item">
+    <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
