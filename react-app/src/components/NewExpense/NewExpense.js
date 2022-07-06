406286ea315fc5dd68ca9937eebc66ae82878066
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-07-06 21:27:39 +0000

    update

---
+++ react-app/src/components/NewExpense/NewExpense.js
@@ -0,0 +1,13 @@
+import React  from "react";
+import './NewExpense.css';
+import ExpenseForm from "./ExpenseForm";
+
+const NewExpense = () => {
+    return (
+        <div className="new-expense">
+            <ExpenseForm />
+        </div>
+    );
+}
+
+export default NewExpense;
