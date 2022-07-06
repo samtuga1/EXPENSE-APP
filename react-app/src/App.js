1113dd9c3f040836361cda0bd836e76426338d38
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-07-06 21:26:20 +0000

    update

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "../src/components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -24,7 +25,7 @@ function App() {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses}/>
     </div>
   );
