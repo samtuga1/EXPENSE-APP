02c1665d0a606e7dbf97538e930dfc67d1052a58
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-07-10 15:23:26 +0000

    update

--- react-app/src/components/NewExpense/NewExpense.js
+++ react-app/src/components/NewExpense/NewExpense.js
@@ -2,10 +2,17 @@ import React  from "react";
 import './NewExpense.css';
 import ExpenseForm from "./ExpenseForm";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random(),
+        }
+        props.NewExpense(expenseData);
+    }
     return (
         <div className="new-expense">
-            <ExpenseForm />
+            <ExpenseForm onAddExpense = {saveExpenseDataHandler} />
         </div>
     );
 }
