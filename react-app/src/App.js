41ef4f9b4c2c4ad8ec7959cb84f3007add15babe
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-07-11 22:28:34 +0000

    update

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,27 +3,7 @@ import Expenses from "../src/components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
-  const expensesList = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const expensesList = [];
   const [expenses, setInitialExpenses] = useState(expensesList);
   const addNewExpenseHandler = (expense) => {
     setInitialExpenses((prevExpenses) => [
